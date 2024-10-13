9ba9864922a081408785844e9463f075b7269763
Shachi-git <delacruz.johnpaulstar@gmail.com>
2024-10-13 09:10:11 +0800

    AM-0003 updated routing for cleaner routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View } from 'react-native'
-import CreateAccount from './(tabs)/createAccount'
+import CreateAccount from './(tabs)/createAccount/index'
 
 export default function App() {
   return (
